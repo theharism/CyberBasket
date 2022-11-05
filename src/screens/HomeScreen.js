729b1d2e565293c893880adfb10e8f91f5344ecf
Huzaifa Iftikhar <chhuzaifaiftikhar@gmail.com>
2022-11-04 23:51:33 -0400

    startup

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,53 +1,56 @@
 import React, { useEffect, useState } from "react";
-import { View,Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import { render } from "react-native-web";
 import Items from "../componenets/Items";
 import SearchBar from "../componenets/SearchBar";
 import useResults from "../hooks/useResults";
 
-const HomeScreen=({navigation})=>{
+const HomeScreen = ({ navigation }) => {
+  const [term, setTerm] = useState("");
+  const [SearchApi, results, errorMessage] = useResults(term);
 
-    const [term,setTerm] = useState('');
-    const [SearchApi,results,errorMessage] = useResults(term);
+  return (
+    <View style={styles.BackgroundStyle}>
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        submitTerm={() => {}}
+      />
 
-    return <View style={styles.BackgroundStyle}>
-       
-       <SearchBar 
-       term={term}
-       onTermChange={(newTerm) => setTerm(newTerm)}
-       submitTerm = {() => {}}
-        />      
- 
-       {errorMessage ? <Text style={{top:40,left:25,position:'relative'}}>{errorMessage}</Text> : null}
+      {errorMessage ? (
+        <Text style={{ top: 40, left: 25, position: "relative" }}>
+          {errorMessage}
+        </Text>
+      ) : null}
 
-       <Text style={{top:50,left:25}}>we have found {results.length} Results</Text>
+      <Text style={{ top: 50, left: 25 }}>
+        Have Founded ! {results.length} Results
+      </Text>
 
-       <View style={{flexDirection:'row',justifyContent:'center',marginHorizontal:10,marginTop:70,flex:1}}>
-        
-       <Items 
-        results={results} 
-        navigation={navigation}
-        />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          marginHorizontal: 10,
+          marginTop: 70,
+          flex: 1,
+        }}
+      >
+        <Items results={results} navigation={navigation} />
 
-        <Items 
-        results={results}
-        navigation={navigation}
-        />
-         
-       </View> 
-
+        <Items results={results} navigation={navigation} />
+      </View>
     </View>
+  );
+};
 
-}
-
-const styles=StyleSheet.create({
-
-BackgroundStyle:{
-    backgroundColor:'rgb(223,226,227)',
-    flex:1,
-    width:"100%"
-}
-
+const styles = StyleSheet.create({
+  BackgroundStyle: {
+    backgroundColor: "rgb(225,227,226)",
+    flex: 1,
+    width: "100%",
+    index: 11,
+  },
 });
 
-export default HomeScreen
+export default HomeScreen;
